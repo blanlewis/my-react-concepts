d33769f810b86de2a5591ce69a9e07fb3f5ee7c3
Blan Lewis <blewi@eg.dk>
2026-06-15 22:06:24 +0530

    Update MapComponent.tsx

--- components/MapComponent/MapComponent.tsx
+++ components/MapComponent/MapComponent.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { MapContainer } from 'react-leaflet/MapContainer'
 import { TileLayer } from 'react-leaflet/TileLayer'
 import { useMap } from 'react-leaflet/hooks'
@@ -5,18 +6,29 @@ import { Marker } from 'react-leaflet/Marker'
 import { Popup } from 'react-leaflet/Popup'
 import 'leaflet/dist/leaflet.css';
 
+const InvalidateSizeOnResize = () => {
+    const map = useMap();
+    useEffect(() => {
+        const observer = new ResizeObserver(() => map.invalidateSize());
+        observer.observe(map.getContainer());
+        return () => observer.disconnect();
+    }, [map]);
+    return null;
+};
+
 const MapComponent = () => {
     return (
     <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: "100%", width: "100%" }}>
-    <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-    />
-    <Marker position={[51.505, -0.09]}>
-        <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-    </Marker>
+        <InvalidateSizeOnResize />
+        <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+            <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+        </Marker>
     </MapContainer>
     );
 }
